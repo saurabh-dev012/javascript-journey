fecf8c6fde67f0fe4b33d7baa819e349277390e7
saurabh-dev012 <saurabh-dev012@users.noreply.github.com>
2026-07-29 18:56:40 +0530

    Add Practice-Problems/objects/q3.js - nested object access practice

---
+++ Practice-Problems/objects/q3.js
@@ -0,0 +1,10 @@
+// From the object below , print the latitude:
+
+const locations = {
+    city : "Bhopal",
+    coordinates: {
+        lat : 23.2,
+        lng : 77.4,
+    },
+};
+locations.coordinates.lat
