098e60fb0eed39d72c023e0fda035389f9e40bd4
saurabh-dev012 <saurabh-dev012@users.noreply.github.com>
2026-07-30 16:41:35 +0530

    feat(dom): add task3 - script to replace paragraph content with innerHTML

---
+++ Practice-Problems/dom/task3/script.js
@@ -0,0 +1,8 @@
+// Select a paragraph and replace its content with:
+{
+     /* <b>Updated</b> by JavaScript */ 
+    
+    }
+
+let paragraph = document.querySelector("p");
+paragraph.innerHTML = "<b>Updated</b> by JavaScript";
