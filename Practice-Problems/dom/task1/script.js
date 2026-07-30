015503adfe4ce0b531a7695cb66295adaa61cf45
saurabh-dev012 <saurabh-dev012@users.noreply.github.com>
2026-07-30 16:22:36 +0530

    feat(dom): add task1 - script to change heading text to Welcome to Sheryians

---
+++ Practice-Problems/dom/task1/script.js
@@ -0,0 +1,4 @@
+// T1: Select the heading of a page by id and change its text to "Welcome to Sheryians!".
+
+let heading = document.querySelector("#heading");
+heading.textContent = "Welcome to Sheryians!";
