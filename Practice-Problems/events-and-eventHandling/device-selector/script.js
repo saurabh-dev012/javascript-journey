5a88c0ff5aab59abede19e4c81711d43d332fae8
saurabh-dev012 <saurabh-dev012@users.noreply.github.com>
2026-07-31 13:05:43 +0530

    Add Practice-Problems\events-and-eventHandling\device-selector\script.js

---
+++ Practice-Problems/events-and-eventHandling/device-selector/script.js
@@ -0,0 +1,5 @@
+let sel = document.querySelector("select");
+let device = document.querySelector("#device");
+sel.addEventListener("change",function(dets){
+    device.textContent = `${dets.target.value} Device Selected`;
+});
