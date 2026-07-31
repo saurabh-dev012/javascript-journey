03b90d56887039ff213747432a4a96fe0e94b3b7
saurabh-dev012 <saurabh-dev012@users.noreply.github.com>
2026-07-31 14:18:27 +0530

    Add Practice-Problems/localStorage-sessionStorage-cookies/1.js

---
+++ Practice-Problems/localStorage-sessionStorage-cookies/1.js
@@ -0,0 +1,18 @@
+// LocalStorage >>   ~5Mb
+
+// for setting or updating an item. setItem overwrites the original values
+
+/* localStorage.setItem("name" , "saurabh"); */
+
+// for fetching an item
+
+/* let val = localStorage.getItem("name");  */
+
+// removing an item
+
+/* localStorage.removeItem("name");  */
+
+// for clearing everything
+
+/* localStorage.clear(); */
+
